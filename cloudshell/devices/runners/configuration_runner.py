59b5ba509d211b0a76228301d27c941af0af7e0c
alexquali <alexey.b@quali.com>
2017-04-04 16:08:06 +0300

    Added support for Firewall device family

--- cloudshell/devices/runners/configuration_runner.py
+++ cloudshell/devices/runners/configuration_runner.py
@@ -75,7 +75,8 @@ class ConfigurationRunner(ConfigurationOperationsInterface):
         :rtype: OrchestrationSavedArtifact or str
         """
 
-        vrf = vrf_management_name or self.resource_config.vrf_management_name
+        if hasattr(self.resource_config, "vrf_management_name"):
+            vrf_management_name = vrf_management_name or self.resource_config.vrf_management_name
 
         self._validate_configuration_type(configuration_type)
         folder_path = self.get_path(folder_path)
@@ -86,7 +87,7 @@ class ConfigurationRunner(ConfigurationOperationsInterface):
         folder_path = self.get_path(full_path)
         self.save_flow.execute_flow(folder_path=folder_path,
                                     configuration_type=configuration_type.lower(),
-                                    vrf_management_name=vrf)
+                                    vrf_management_name=vrf_management_name)
 
         if return_artifact:
             artifact_type = full_path.split(':')[0]
@@ -104,14 +105,15 @@ class ConfigurationRunner(ConfigurationOperationsInterface):
         :return: exception on crash
         """
 
-        vrf = vrf_management_name or self.resource_config.vrf_management_name
+        if hasattr(self.resource_config, "vrf_management_name"):
+            vrf_management_name = vrf_management_name or self.resource_config.vrf_management_name
 
         self._validate_configuration_type(configuration_type)
         path = self.get_path(path)
         self.restore_flow.execute_flow(path=path,
                                        configuration_type=configuration_type.lower(),
                                        restore_method=restore_method.lower(),
-                                       vrf_management_name=vrf)
+                                       vrf_management_name=vrf_management_name)
 
     def orchestration_save(self, mode="shallow", custom_params=None):
         """Orchestration Save command
